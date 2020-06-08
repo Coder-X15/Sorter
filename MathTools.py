bfc10477939b0d49d1039b3c00bf29d16e39828a
Coder-X15 <63494859+Coder-X15@users.noreply.github.com>
2020-06-08 17:18:30 +0530

    Add files via upload

--- MathTools.py
+++ MathTools.py
@@ -1,39 +1,37 @@
 def factorize(x):
-    # function for factorisation (not prime factorisation)
-    u = set([1,int(x)]) # a set of factors (initially, 1 and the number itself)
+    u = set([1,int(x)])
     for i in range(1,int(x)):
-	# in this loop, we try to find two numbers that can be used to express the number to be factorised
-        if x % i == 0: # since factors leave 0 remainder
-            u.add(x/i) # the factor no.1
-            u.add(i) # the factor no.2
-    return u # set of factors return 
-def HCF(*num):
-    # HCF (highest common factor) = GCD(greatest common divisor)
+        if x % i == 0:
+            u.add(x/i)
+            u.add(i)
+    return u
+def hcf(*num):
     u = []
+    if type(num[0]) == list:
+        num = num[0]
     for i in num:
-        u.append(factorize(i)) # set of factors of each number appended to list
+        u.append(factorize(i))
     v = u[0]
     for j in range(1,len(u)):
-        v = v.intersection(u[j]) # filtering out common factors culminatively
+        v = v.intersection(u[j])
     if len(v) == 2:
-        return list(v - {1})[0] # 1 is removed, since it is a factor of every number 
+        return list(v - {1})[0]
     else:
-        y = max(v) # returns the highest factor in the set
+        y = max(v)
         return y
+def gcd(*num):
+    return hcf(list(num))
 
+def lcm(*num):
+    num = list(num)
+    hcf = HCF(num)
+    for  i in range(len(num)):
+        num[i] *= 1/hcf
+    backup = tuple(num)
+    exit_stat = False
+    while exit_stat is False:
+        num[num.index(min(num))] += backup[num.index(min(num))]
+        if num.count(num[0]) == len(num):
+            exit_stat = True
+            return num[0] * hcf
 
-def LCM(*num):
-	num = list(num) 
-	u = []
-	logic = [] # logic list: to find if a number appears as a multiple in the list of multiples of other numbers
-	for n in num:
-		u.append(list(n * i for  i in range(1, 101)))
-	for j in u[0]:
-		logic = []
-		for k in range(1, len(u)):
-			if j in u[k]: # checks if j (in the multiples of the first number in the 'num' argument) appears as a multiple among the multiples of other numbers in the tuple
-				logic.append(True)
-		if logic.count(True) == len(u) - 1:
-			break
-	return j # returns the common number
-# please do modify it if bugs are found...
