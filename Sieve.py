c49dfa124e03c46abe640804cecf8988b7be6b8f
Sam Ruben Abraham <63494859+Coder-X15@users.noreply.github.com>
2020-10-28 19:14:36 +0530

    Update Sieve.py

--- Sieve.py
+++ Sieve.py
@@ -4,11 +4,11 @@ class PrimeNumSieve:
     def __init__(self, limit):
         self.temp = []
         for i in range(1, limit):
-	if mt.factorial(i) % i**2 != 0:
-		self.temp.append(i)
+		if mt.factorial(i) % i**2 != 0:
+			self.temp.append(i)
     def __call__(self):
         self.output = []
         for j in range(len(self.temp)):
-	if len(mt.factorize(self.temp[j])) == 2:
-		sieve.append(self.temp[j])
+		if len(mt.factorize(self.temp[j])) == 2:
+			sieve.append(self.temp[j])
 	return self.output
